5c2fe9ee081178a2de05226d2a7893a566955125
racer-prog <topgear35rus@ya.ru>
2024-09-12 12:03:26 +0300

    finish commit

--- module_3_3.py
+++ module_3_3.py
@@ -7,7 +7,14 @@ def print_params(a = 1, b = 'строка', c = True):
 values_list = [1,'str',True]
 values_dict = {'a': 2, 'b': 'str2', 'c': True}
 
+values_list_2 = [1, True]
 
 #print_params()
 print_params(*values_list)
 print_params(**values_dict)
+
+print_params(*values_list_2, 42)
+
+print_params(b = 25)
+
+print_params(c = [1,2,3])
