90296a55c3cd1baf2a94b4d18c37269d038c6c7f
racer-prog <topgear35rus@ya.ru>
2024-09-19 17:59:15 +0300

    finish

---
+++ module_4_2.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    return inner_function()
+
+
+
+
+if __name__ == "__main__":
+    test_function()
