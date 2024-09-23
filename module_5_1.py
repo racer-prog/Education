99d91eb121c51a2309ba2bc74bf71fbcbbc369fe
racer-prog <topgear35rus@ya.ru>
2024-09-23 22:55:46 +0300

    first commit

---
+++ module_5_1.py
@@ -0,0 +1,16 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        new_floor = int(new_floor)
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor):
+                print(i)
+
+if __name__ == "__main__":
+    my_house = House("IT_PARK",17)
+    my_house.go_to(17)
