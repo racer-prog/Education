c90980287fc3d5e9de32b54df6b0dda97453bd42
racer-prog <topgear35rus@ya.ru>
2024-10-02 22:44:26 +0300

    finish

---
+++ module_5_4.py
@@ -0,0 +1,39 @@
+#from module_5_1 import House
+#from module_5_3 import House_
+
+
+class House():
+
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return  object.__new__(cls)
+
+    def __init__(self, *args):
+        self.name = args[0]
+        self.number_of_floors = args [1]
+        self.args = args
+
+
+
+
+    def __del__(self, *args):
+        print(f"{self.name} снесён, но он останется в истории")
+
+        #return self.houses_history
+
+
+
+if __name__ == "__main__":
+    h1 = House('ЖК Эльбрус', 10)
+    print(House.houses_history)
+    h2 = House('ЖК Акация', 20)
+    print(House.houses_history)
+    h3 = House('ЖК Матрёшки', 20)
+    print(House.houses_history)
+
+    del h2
+    del h3
+
+    print(House.houses_history)
