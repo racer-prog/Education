f4cc7fdbd3d5612c0798f1881dc961f9f6bfa993
racer-prog <topgear35rus@ya.ru>
2024-10-14 00:07:28 +0300

    pochti_finish

---
+++ module_6_2.py
@@ -0,0 +1,59 @@
+class Vehicle: #любой транспорт
+
+    owner = str #владелец
+    _model = str
+    __engine_power = int
+    __color = str
+    __COLOR_VARIANTS = []
+
+    def __init__(self, owner_:str, model_:str, color_:str, engine_power_:int):
+        self.owner = owner_
+        self._model = model_
+        self.__color = color_
+        self.__engine_power = engine_power_
+
+    def set_color(self, new_color:str):
+        print(new_color, self.__color)
+        print(str(any(self.__COLOR_VARIANTS)).lower())
+        new_color_ = str
+        for color_ in self.__COLOR_VARIANTS:
+            if new_color.lower() == str(color_).lower():
+                new_color_ = new_color.lower()
+        if new_color_ in self.__COLOR_VARIANTS:
+            self.__color = new_color_
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print("Владелец:", self.owner)
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_model(self):
+        return f"Модель: {self._model}"
+
+class Sedan(Vehicle): #седан, наследник Vehicle
+    __PASSENGERS_LIMIT = 5
+
+if __name__ == "__main__":
+    # Текущие цвета
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+    # Изначальные свойства
+    vehicle1.print_info()
+
+    # Меняем свойства (в т.ч. вызывая методы)
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
+
+    # Проверяем что поменялось
+    vehicle1.print_info()
