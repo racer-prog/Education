41d146504e8620ce95ed60f6e954aea46631c53c
racer-prog <topgear35rus@ya.ru>
2024-10-30 19:26:01 +0300

    finish

--- module_7_file_in_os.py
+++ module_7_file_in_os.py
@@ -1,52 +1,21 @@
 import os, time
 
-# list_all_files = []
-# list_dir = []
-# list_files = []
-#
-# def add_all_files(dir_:str):
-#     list_files = os.listdir(dir_)
-#     print(list_files)
-#     for name_ in list_files:
-#         list_all_files.append(os.path.abspath(name_))
-#     # return list_files
-#
-# def sort_names(list_:[]):
-#     while len(list_all_files) != 0:
-#         for name_ in list_:
-#             if os.path.isdir(name_) == True:
-#                 # print(f"{name_} - Директория")
-#                 list_dir.append(name_)
-#                 list_all_files.remove(name_)
-#
-#             else:
-#                 # print(f"{name_} - Файл")
-#                 list_files.append(name_)
-#                 list_all_files.remove(name_)
-
-
-
-
 if __name__ == "__main__":
-    # add_all_files('.')
-    # sort_names(list_all_files)
-    # print('Директории: ', list_dir)
-    # print('Файлы: ',list_files)
-    # print(list_all_files)
 
-    directory = '.'
+    directory = os.getcwd()
+    # directory = ".."
     list_files = os.walk(directory)
-    # print(list_files.)
 
     for root, dirs, files in os.walk(directory):
         for file in files:
-            filepath = os.path.abspath(file)
 
-            filetime = os.path.getatime(file)
+            filepath = os.path.join(root,file)
+
+            filetime = os.path.getatime(filepath)
 
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 
-            filesize = os.path.getsize(file)
+            filesize = os.path.getsize(filepath)
 
             parent_dir = os.path.dirname(filepath)
 
