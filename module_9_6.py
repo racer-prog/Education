abe090e61116fd8e3679499f34fa8d030aacd14b
racer-prog <topgear35rus@ya.ru>
2024-11-20 13:23:59 +0300

    finish

--- module_9_6.py
+++ module_9_6.py
@@ -2,7 +2,18 @@ def all_variants(text):
 
     for x in range(len(text)):
         for y in range(len(text)-x):
-            yield text[x:y+x+1]
+            yield text[y:y+x+1]
+
+def all_variants2(text):
+    i = 0
+    for x in range(len(text)):
+        # print(len(text), "len(text)")
+        for y in range(len(text)-x):
+            print("Result:", "'",text[y:y+x+1],"'", "при X,Y:", x,y)
+            i += 1
+            # if i >= (len(text)) * 2 - 1:
+            #     break
+
 
 
 
@@ -11,4 +22,9 @@ if __name__ == "__main__":
     a = all_variants("abc")
     for i in a:
         print(i)
+
+    #all_variants2("abc")
+    # all_variants2("abcd")
+
+
 
