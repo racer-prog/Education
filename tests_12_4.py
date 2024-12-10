ce782beadb2097981a0848fb421ef020ff63d40e
racer-prog <topgear35rus@ya.ru>
2024-12-10 23:04:47 +0300

    finish

---
+++ tests_12_4.py
@@ -0,0 +1,6 @@
+import tests_12_3 as test
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="runner_tests.log",
+                    filemode="w", encoding="UTF-8",
+                    format="%(levelname)s -> %(message)s")
