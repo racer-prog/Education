9c5df1b439935d7d1f667ea21fb082d7652003c5
racer-prog <topgear35rus@ya.ru>
2024-12-13 17:37:38 +0300

    finish_

--- module_13_1.py
+++ module_13_1.py
@@ -4,10 +4,19 @@ import time
 async def start_strongman(name, power):
     n = 5
     print(f'Силач {name} начал соревнования.')
+    for i in range(n):
+        print(f'Силач {name} поднял {i+1} шар')
+        await asyncio.sleep((1/power)*20)
     print(f'Силач {name} закончил соревнования.')
 
 async def start_tournament():
-    pass
+    task_1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task_2 = asyncio.create_task(start_strongman('Denis', 4))
+    task_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task_1
+    await task_2
+    await task_3
 
 
-start_tournament().run
+
+asyncio.run(start_tournament())
