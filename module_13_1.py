0c000cfd398b741146fa37c9a76596160bc9c866
racer-prog <topgear35rus@ya.ru>
2024-12-13 17:04:30 +0300

    first_

---
+++ module_13_1.py
@@ -0,0 +1,13 @@
+import asyncio
+import time
+
+async def start_strongman(name, power):
+    n = 5
+    print(f'Силач {name} начал соревнования.')
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    pass
+
+
+start_tournament().run
